ef413b238150b4c47f3d646d43f95cac96c5b787
joseallef <46696111+joseallef@users.noreply.github.com>
2020-10-27 19:17:20 -0300

    Adicionando o front end

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles/global.css';
+import 'leaflet/dist/leaflet.css';
+
+import Routes from './routes';
+
+
+// JSX JavaScript XML
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
